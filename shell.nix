fc371a846d906430b2b0b04ae0e2987fb633dbb9
Miguel Villa Floran <miguel.villafloran@gmail.com>
2026-04-03 16:05:09 -0700

    Pin to specific mjlab version

--- shell.nix
+++ shell.nix
@@ -2,17 +2,23 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+let
+  python = pkgs.python312;
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python312 # Python 3.12
-    uv # Python package manager
-    nixfmt # Nix formatter
-    just # Just
+    python
+    uv
+    nixfmt
+    just
+    stdenv.cc.cc.lib
   ];
 
   # Shell hook to set up environment
   shellHook = ''
     export TMPDIR=/tmp
+    export UV_PYTHON="${python}/bin/python"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
     just install
   '';
 }
